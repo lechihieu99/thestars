83032d28529b28fb07c8bbd0b9f7fa2f7b91005c
lechihieu99 <lechihieu99us@gmail.com>
2024-01-10 08:26:54 +0700

    change url

--- src/app/components/section3/Section3.jsx
+++ src/app/components/section3/Section3.jsx
@@ -17,7 +17,7 @@ import Card from "../card/Card";
 import axios from "axios";
 
 const axiosApi = axios.create({
-    baseURL: 'https://f5c6-14-169-3-129.ngrok-free.app/api',
+    baseURL: 'https://ba4f-171-245-138-173.ngrok-free.app/api',
     headers: {
         'Content-type': 'application/json',
         "ngrok-skip-browser-warning": "69420",
